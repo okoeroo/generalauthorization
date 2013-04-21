a9ecfee67818521cd4999b7bb9f05ef1562ff50f
Oscar Koeroo <okoeroo@gmail.com>
2013-04-21 10:25:52 +0200

    Exchanging expat for libxml2

--- src/genauthz_pdp.c
+++ src/genauthz_pdp.c
@@ -15,11 +15,110 @@
 #include "genauthz_httprest.h"
 #include "genauthz_pdp.h"
 
+
+#include <string.h>
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+
+void trim(char *str)
+{
+    int i;
+
+    for (i = strlen(str) - 1; 0 <= i; i--) {
+        if ((str[i] == '\r') ||
+            (str[i] == '\n') ||
+            (str[i] == ' ')) {
+            str[i] = '\0';
+        }
+        else {
+            break;
+        }
+    }
+}
+
+void walk(xmlNodePtr node, int depth)
+{
+    char     buf[64];
+    char     content[128];
+    xmlNode *cur_node = NULL;
+    int i = 0;
+
+    /* Indent space */
+    for (i = 0; i < (depth * 2); i++) {
+        buf[i] = ' ';
+    }
+    buf[i] = '\0';
+
+    for (cur_node = node; cur_node; cur_node = cur_node->next) {
+        printf("%s", buf);
+
+        /* Type */
+        printf("type(%d):", cur_node->type);
+        switch (cur_node->type) {
+            case XML_ELEMENT_NODE:
+                printf("Node, ");
+                break;
+            case XML_ATTRIBUTE_NODE:
+                printf("Attribute, ");
+                break;
+            case XML_TEXT_NODE:
+                printf("Text, ");
+                break;
+            default:
+                printf("Other, ");
+                break;
+        }
+
+        /* Name of node, or entity */
+        printf("name:%s, ", cur_node->name);
+
+        /* Content */
+        if (NULL == cur_node->content) {
+            printf("content:(null)");
+        }
+        else {
+            strcpy(content, cur_node->content);
+            trim(content);
+            printf("content:%s", content);
+        }
+        printf("\n");
+
+        walk(cur_node->children, depth + 1);
+    }
+}
+
 static evhtp_res
 pdp_xml_processor(evhtp_request_t *req) {
     evhtp_res http_res = EVHTP_RES_SERVERR;
+    unsigned char *buf = NULL;
+    size_t bufsize = 0;
+    xmlDocPtr  doc;
+    xmlNodePtr root_element = NULL;
 
-    syslog(LOG_DEBUG, "%s", __func__);
+    syslog(LOG_DEBUG, "%s: %s", __func__, buf);
+    LIBXML_TEST_VERSION;
+
+    /* Read document */
+    doc = xmlReadMemory(evpull(req->buffer_in),
+                        evbuffer_get_length(req->buffer_in),
+                        NULL,
+                        NULL,
+                        0);
+    if (doc == NULL) {
+        fprintf(stderr, "Failed to parse\n");
+        return;
+    }
+
+    /*Get the root element node */
+    root_element = xmlDocGetRootElement(doc);
+
+    /* Walk child */
+    walk(root_element, 0);
+    http_res = EVHTP_RES_200;
+
+    /* Free document */
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
 
     return http_res;
 }
