181903fbf8490c542f61e3293f050988013a13b4
Oscar Koeroo <okoeroo@gmail.com>
2013-04-27 21:36:07 +0200

    Added gnuplot and benchmark updates

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
 BENCHMARK_OUT_DIR="/tmp/benchmark_output"
+TRIES=1
+WAITSEC=5
+TIMEOUT=30
+#TARGET="http://127.0.0.1:8081/authorization/pdp/"
+TARGET="http://172.16.65.1:8081/authorization/pdp/"
 
 benchrun() {
 
     if [ ! -d "${BENCHMARK_OUT_DIR}" ]; then
         mkdir "${BENCHMARK_OUT_DIR}"
     fi
-    ab -t 10 -c $1 -n $2 \
+    ab -c $1 -n $2 \
         -g "${BENCHMARK_OUT_DIR}/ga_$1_$2_run_$3.gnuplot" \
             -p xacml_request.xml \
             -H "Accept: application/xacml+xml" \
-            http://127.0.0.1:8081/authorization/pdp/;
+            "${TARGET}";
 }
 
 bench() {
     for ((i=1;i<=$3;i++)); do
         benchrun $1 $2 $i
-        sleep 3
+        echo "Waiting ${WAITSEC} seconds"
+        sleep ${WAITSEC}
     done
-    sleep 3
+    echo "Waiting ${WAITSEC} seconds"
+    sleep ${WAITSEC}
 }
 
 # Concurrency 1, total calls 100, amount of these runs 10
-TRIES=10
 
-bench   8  5000 ${TRIES}
-bench  16  2000 ${TRIES}
-bench  64  2000 ${TRIES}
-bench 100  2000 ${TRIES}
-bench  10 10000 ${TRIES}
+bench   8   5000 ${TRIES}
+bench  16   5000 ${TRIES}
+#bench  16   2000 ${TRIES}
+#bench  64   2000 ${TRIES}
+#bench 100   2000 ${TRIES}
+#bench   8 100000 ${TRIES}
+#bench   8 1000000 ${TRIES}
 
---
+++ tests/plot_it.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIR="/tmp/benchmark_output"
+OPTION="using 9 smooth sbezier with lines"
+TMPFILE="/tmp/tmp_plot_file.tmp"
+
+PLOTLINE=""
+i=0
+
+cat > "$TMPFILE" <<End-of-message
+set title "XACML GA tests"
+set ylabel "response time (ms)"
+set xlabel "request"
+set terminal pngcairo size 1024,768 enhanced font 'Verdana,12'
+set output 'xacml_ga_plot.png'
+End-of-message
+
+ls "${DIR}" | while read FILE; do
+    i=`expr $i + 1`
+    if [ $i -eq 1 ]; then
+        echo -n "plot \"${DIR}/${FILE}\" $OPTION" >> "$TMPFILE"
+    else
+        echo -n ", \"${DIR}/${FILE}\" $OPTION" >> "$TMPFILE"
+    fi
+done
+echo >> "$TMPFILE"
+
+cat "$TMPFILE"
+
