87664778977ebb42a2681b3e07c5521aaeab49c8
Oscar Koeroo <okoeroo@gmail.com>
2013-04-28 09:14:16 +0200

    Tweaking the benchmarks

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -6,21 +6,34 @@ WAITSEC=5
 TIMEOUT=30
 #TARGET="http://127.0.0.1:8081/authorization/pdp/"
 TARGET="http://172.16.65.1:8081/authorization/pdp/"
-PREFIX="ga_threads_8"
+PREFIX="ga_threads_16"
 
 PLOT_OPTION="using 9 smooth sbezier with lines"
 TMPFILE="/tmp/tmp_plot_file.tmp"
 PNG="xacml_ga_plot.png"
 PLOTLINE=""
-i=0
 
+
+thresholding() {
+    THRESHOLD=10;
+    while [ true ]; do
+        TWCNT=$(netstat -na | grep TIME_WAIT | wc -l);
+        if [ $TWCNT -lt $THRESHOLD ]; then
+            break;
+        else
+            echo "Still seeing $TWCNT TIME_WAIT. Waiting until less then $THRESHOLD";
+            sleep 3;
+        fi;
+    done
+}
 
 benchrun() {
+    thresholding
 
     if [ ! -d "${BENCHMARK_OUT_DIR}" ]; then
         mkdir "${BENCHMARK_OUT_DIR}"
     fi
-    ab -c $1 -n $2 \
+    ab -r -c $1 -n $2 \
         -g "${BENCHMARK_OUT_DIR}/${PREFIX}_$1_$2_run_$3.gnuplot" \
             -p xacml_request.xml \
             -H "Accept: application/xacml+xml" \
@@ -38,11 +51,12 @@ bench() {
 # Concurrency 1, total calls 100, amount of these runs 10
 
 #bench    8  10000 ${TRIES}
-#bench   16  20000 ${TRIES}
-#bench   32  10000 ${TRIES}
-#bench   64  10000 ${TRIES}
+bench   16  10000 ${TRIES}
+bench   32  10000 ${TRIES}
+bench   64  100000 ${TRIES}
 bench  128  100000 ${TRIES}
-#bench  256  10000 ${TRIES}
+bench  256  50000 ${TRIES}
+bench  512  1000 ${TRIES}
 #bench    8 100000 ${TRIES}
 #bench    8 1000000 ${TRIES}
 
@@ -57,6 +71,7 @@ set terminal pngcairo size 1024,768 enhanced font 'Verdana,12'
 set output '$PNG'
 End-of-message
 
+i=0
 ls "${BENCHMARK_OUT_DIR}" | while read FILE; do
     i=`expr $i + 1`
     if [ $i -eq 1 ]; then
--- tests/plot_it.sh
+++ tests/plot_it.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="/tmp/benchmark_output"
-OPTION="using 9 smooth sbezier with lines"
+PLOT_OPTION="using 9 smooth sbezier with lines"
 TMPFILE="/tmp/tmp_plot_file.tmp"
 
 PNG="xacml_ga_plot.png"
@@ -13,16 +13,16 @@ cat > "$TMPFILE" <<End-of-message
 set title "XACML GA tests"
 set ylabel "response time (ms)"
 set xlabel "request"
-set terminal pngcairo size 1024,768 enhanced font 'Verdana,12'
+set terminal pngcairo size 1366,900 enhanced font 'Verdana,12'
 set output '$PNG'
 End-of-message
 
-ls "${DIR}" | while read FILE; do
+ls ${DIR}/ga_threads_{8,16,32}_256*  | while read FILE; do
     i=`expr $i + 1`
     if [ $i -eq 1 ]; then
-        echo -n "plot \"${DIR}/${FILE}\" $OPTION" >> "$TMPFILE"
+        echo -n "plot \"${FILE}\" $PLOT_OPTION" >> "$TMPFILE"
     else
-        echo -n ", \"${DIR}/${FILE}\" $OPTION" >> "$TMPFILE"
+        echo -n ", \"${FILE}\" $PLOT_OPTION" >> "$TMPFILE"
     fi
 done
 echo >> "$TMPFILE"
