159017e168a8a820cd685e7e3511d5e49012b68e
Oscar Koeroo <okoeroo@gmail.com>
2013-05-12 00:05:28 +0200

    Added JSON output to a Response message. Warning! Advices, IncludeInResult and Obligations are also added and as of to date not part of the spec yet. Also added TAILQ_COUNT(_SAFE).

--- src/genauthz_json_xacml.c
+++ src/genauthz_json_xacml.c
@@ -12,6 +12,186 @@
 #include <string.h>
 
 
+
+int
+normalized_xacml_attribute_values2json_evbuffer(struct evbuffer *output,
+                                                tq_xacml_attribute_value_list_t attr_value_list) {
+    struct tq_xacml_attribute_value_s *value;
+    int    first;
+    int    c;
+
+    TAILQ_COUNT_SAFE(c, value, &attr_value_list, next);
+
+    if (c == 0) {
+        return 0;
+    } else if (c == 1) {
+        TAILQ_FOREACH(value, &attr_value_list, next) {
+            if (value->datatype == GA_XACML_DATATYPE_STRING) {
+                evbuffer_add_printf(output, "            \"Value\": \"%s\"",
+                                            (char *)value->data);
+            } else if (value->datatype == GA_XACML_DATATYPE_INTEGER) {
+                evbuffer_add_printf(output, "            \"Value\": %d",
+                                            (int32_t)value->data);
+            }
+        }
+    } else {
+        evbuffer_add_printf(output, "            \"Value\": [");
+
+        first = 1;
+        TAILQ_FOREACH(value, &attr_value_list, next) {
+            first ? first = 0 : evbuffer_add_printf(output, ",");
+
+            if (value->datatype == GA_XACML_DATATYPE_STRING) {
+                evbuffer_add_printf(output, "\"%s\"",
+                                            (char *)value->data);
+            } else if (value->datatype == GA_XACML_DATATYPE_INTEGER) {
+                evbuffer_add_printf(output, "%d",
+                                            (int32_t)value->data);
+            }
+        }
+        evbuffer_add_printf(output, "]");
+    }
+
+    return 0;
+}
+
+int
+normalized_xacml_attributes2json_evbuffer(struct evbuffer *output,
+                                          tq_xacml_attribute_list_t attr_list) {
+    struct tq_xacml_attribute_s *x_attribute;
+    int    first = 1;
+    int    c;
+
+    /*
+
+        "Attribute": [{
+                    "Id": "urn:oasis:names:tc:xacml:2.0:subject:role",
+                    "Value" : ["manager","administrator"]
+                }]
+
+    "Attribute": {
+        "Id"        : "document-id"
+        "DataType"  : "integer"
+              "Value"   : 123
+    }
+    */
+
+    TAILQ_COUNT_SAFE(c, x_attribute, &attr_list, next);
+
+    if (c == 0) {
+        return 0;
+    } else if (c == 1) {
+        evbuffer_add_printf(output, "        \"Attribute\": {\n");
+        TAILQ_FOREACH(x_attribute, &attr_list, next) {
+            evbuffer_add_printf(output, "            \"Id\": \"%s\"",
+                                        x_attribute->id);
+            /* Output for the Attribute values */
+            if (!(TAILQ_EMPTY(&(x_attribute->values)))) {
+                evbuffer_add_printf(output, ",\n");
+                normalized_xacml_attribute_values2json_evbuffer(output, x_attribute->values);
+                evbuffer_add_printf(output, "\n");
+            } else {
+                evbuffer_add_printf(output, "\n");
+            }
+            evbuffer_add_printf(output, "          }");
+        }
+    } else {
+        evbuffer_add_printf(output, "        \"Attribute\": [\n");
+        TAILQ_FOREACH(x_attribute, &attr_list, next) {
+            first ? first = 0 : evbuffer_add_printf(output, ",\n");
+
+            evbuffer_add_printf(output, "          {\n"
+                                        "            \"Id\": \"%s\"",
+                                        x_attribute->id);
+            /* Output for the Attribute values */
+            if (!(TAILQ_EMPTY(&(x_attribute->values)))) {
+                evbuffer_add_printf(output, ",\n");
+                normalized_xacml_attribute_values2json_evbuffer(output, x_attribute->values);
+                evbuffer_add_printf(output, "\n");
+            } else {
+                evbuffer_add_printf(output, "\n");
+            }
+            evbuffer_add_printf(output, "          }");
+        }
+        evbuffer_add_printf(output, "\n        ]");
+    }
+
+    return 0;
+}
+
+int
+normalized_xacml_categories2json_evbuffer(struct evbuffer *output,
+                                          tq_xacml_category_list_t cat_list,
+                                          enum ga_xacml_category_e cat_type) {
+    struct tq_xacml_category_s *category;
+    int    first = 1;
+    int c;
+
+    TAILQ_COUNT_SAFE(c, category, &cat_list, next);
+    if (c == 0) {
+        return 0;
+    } else if (c == 1) {
+        TAILQ_FOREACH(category, &cat_list, next) {
+            if (category->type != cat_type) {
+                continue;
+            }
+
+            switch (category->type) {
+                case GA_XACML_CATEGORY_OBLIGATION:
+                    evbuffer_add_printf(output, "      \"Obligation\" : {\n");
+                    break;
+                case GA_XACML_CATEGORY_ADVICE:
+                    evbuffer_add_printf(output, "      \"Advice\" : {\n");
+                    break;
+                default:
+                    evbuffer_add_printf(output, "ERROR: Internal server error\n");
+                    return 1;
+            }
+            if (!(TAILQ_EMPTY(&(category->attributes)))) {
+                evbuffer_add_printf(output, "        \"Id\" : \"%s\",\n", category->id);
+                normalized_xacml_attributes2json_evbuffer(output, category->attributes);
+                evbuffer_add_printf(output, "\n      }");
+            } else {
+                evbuffer_add_printf(output, "        \"Id\" : \"%s\"\n      }", category->id);
+            }
+        }
+    } else {
+        TAILQ_FOREACH(category, &cat_list, next) {
+            if (category->type != cat_type) {
+                continue;
+            }
+            if (first) {
+                switch (category->type) {
+                    case GA_XACML_CATEGORY_OBLIGATION:
+                        evbuffer_add_printf(output, "      \"Obligation\" : [\n");
+                        break;
+                    case GA_XACML_CATEGORY_ADVICE:
+                        evbuffer_add_printf(output, "      \"Advice\" : [\n");
+                        break;
+                    default:
+                        evbuffer_add_printf(output, "ERROR: Internal server error\n");
+                        return 1;
+                }
+                first = 0;
+            } else {
+                evbuffer_add_printf(output, ",\n");
+            }
+
+            if (!(TAILQ_EMPTY(&(category->attributes)))) {
+                evbuffer_add_printf(output, "{\n        \"Id\" : \"%s\",\n", category->id);
+                normalized_xacml_attributes2json_evbuffer(output, category->attributes);
+            } else {
+                evbuffer_add_printf(output, "{\n        \"Id\" : \"%s\"\n", category->id);
+            }
+
+            evbuffer_add_printf(output, "        }\n");
+        }
+        evbuffer_add_printf(output, "      ]");
+    }
+
+    return 0;
+}
+
 evhtp_res
 pdp_json_output_processor(struct evbuffer *output,
                          struct tq_xacml_response_s *xacml_res) {
@@ -20,40 +200,39 @@ pdp_json_output_processor(struct evbuffer *output,
     /* Response header */
     evbuffer_add_printf(output,
             "{\n"
-            "    \"Response\" : {\n"
-            "        \"Result\" : {\n"
-            "            \"Decision\" : \"%s\"\n"
-            "        }\n"
-            "    }\n"
-            "}\n",
+            "  \"Response\" : {\n"
+            "    \"Result\" : {\n"
+            "      \"Decision\" : \"%s\"",
             xacml_decision2str(xacml_res->decision));
 
-#if 0
     /* Obligations */
     if (!(TAILQ_EMPTY(&(xacml_res->obligations)))) {
-        evbuffer_add_printf(output, "    <Obligations>\n");
-        normalized_xacml_categories2xml_evbuffer(output, xacml_res->obligations);
-        evbuffer_add_printf(output, "    </Obligations>\n");
-    }
-    /* Associated Advice */
-    if (!(TAILQ_EMPTY(&(xacml_res->advices)))) {
-        evbuffer_add_printf(output, "    <AssociatedAdvice>\n");
-        normalized_xacml_categories2xml_evbuffer(output, xacml_res->advices);
-        evbuffer_add_printf(output, "    </AssociatedAdvice>\n");
-    }
-    /* IncludeInResult Attributes */
-    if (!(TAILQ_EMPTY(&(xacml_res->attributes)))) {
-        evbuffer_add_printf(output, "    <Attributes>\n");
-        normalized_xacml_attributes2xml_evbuffer(output, xacml_res->attributes);
-        evbuffer_add_printf(output, "    </Attributes>\n");
+        evbuffer_add_printf(output, ",\n");
+        normalized_xacml_categories2json_evbuffer(output,
+                                                  xacml_res->obligations,
+                                                  GA_XACML_CATEGORY_OBLIGATION);
     }
 
-    /* Finalize */
+    /* Advices */
+    if (!(TAILQ_EMPTY(&(xacml_res->advices)))) {
+        evbuffer_add_printf(output, ",\n");
+        normalized_xacml_categories2json_evbuffer(output,
+                                                  xacml_res->obligations,
+                                                  GA_XACML_CATEGORY_ADVICE);
+    }
+
+    /* IncludeInResult Attributes */
+    if (!(TAILQ_EMPTY(&(xacml_res->attributes)))) {
+        evbuffer_add_printf(output, ",\n");
+        normalized_xacml_attributes2json_evbuffer(output, xacml_res->attributes);
+    }
+
+    /* Finalize the Result object */
     evbuffer_add_printf(output,
-            "  </Result>\n");
-    evbuffer_add_printf(output,
-            "</Response>\n");
-#endif
+            "\n"
+            "    }\n"
+            "  }\n"
+            "}\n");
 
     return http_res;
 }
