bed15d8e790fcea04fce8662a3dc5d8d2248e8de
Oscar Koeroo <okoeroo@gmail.com>
2013-05-31 21:15:37 +0200

    Fixing the plugin header file content, and install all the needed headers in /usr{/local}/include/genauthz/

---
+++ src/genauthz_plugin.h
@@ -0,0 +1,11 @@
+#ifndef GENAUTHZ_PLUGIN_H
+    #define GENAUTHZ_PLUGIN_H
+
+#include <genauthz/genauthz_common.h>
+#include <genauthz/genauthz_xacml.h>
+#include <genauthz/genauthz_normalized_xacml.h>
+#include <genauthz/genauthz_xacml_rule_parser.h>
+#include <genauthz/genauthz_callout_helper.h>
+
+#endif
+
