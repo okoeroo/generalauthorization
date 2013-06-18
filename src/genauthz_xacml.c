f31c7717f5772904b755d6ddd28f5ac9836157ca
Oscar Koeroo <okoeroo@gmail.com>
2013-06-18 14:37:12 +0200

    datatype to ga_xml_datatype.

--- src/genauthz_xacml.c
+++ src/genauthz_xacml.c
@@ -1,107 +1,108 @@
 #include "ga_config.h"
 #include "genauthz/genauthz_xacml.h"
 
+RB_HEAD(ga_xml_datatype_tree, ga_xml_datatype);
 
-static int xdatatype_tree_initialized = 0;
+int ga_xml_datatype_tree_initialized = 0;
+struct ga_xml_datatype_tree ga_xml_datatype_head;
 
-struct datatype {
+
+struct ga_xml_datatype {
+    RB_ENTRY(ga_xml_datatype) entry;
+
     ga_xacml_datatype_t type;
     const char *str;
-
-    RB_ENTRY(datatype) entry;
 };
 
-static int
-datatype_cmp(void * _a, void * _b) {
-    struct datatype * a = _a;
-    struct datatype * b = _b;
+static int ga_xml_datatype_cmp(struct ga_xml_datatype *, struct ga_xml_datatype *);
 
+/* RB_HEAD(datatype_tree, datatype) datatype_head = RB_INITIALIZER(&datatype_head); */
+
+RB_PROTOTYPE(ga_xml_datatype_tree, ga_xml_datatype, entry, ga_xml_datatype_cmp)
+RB_GENERATE(ga_xml_datatype_tree, ga_xml_datatype, entry, ga_xml_datatype_cmp)
+
+#define xml_datatype_add(xdatatype, cstr) do {                        \
+        struct ga_xml_datatype *c;                                    \
+        c = malloc(sizeof(struct ga_xml_datatype));                   \
+        c->type = xdatatype;                                          \
+        c->str  = cstr;                                               \
+                                                                      \
+        RB_INSERT(ga_xml_datatype_tree, &ga_xml_datatype_head, c);    \
+} while (0)
+
+static int
+ga_xml_datatype_cmp(struct ga_xml_datatype *a, struct ga_xml_datatype *b) {
     return b->type - a->type;
 }
 
-
-RB_HEAD(datatype_tree, datatype) datatype_head = RB_INITIALIZER(&datatype_head);
-
-RB_PROTOTYPE_STATIC(datatype_tree, datatype, entry, datatype_cmp)
-RB_GENERATE_STATIC(datatype_tree, datatype, entry, datatype_cmp)
-
-#define xdatatype_add(xdatatype, cstr) do {                    \
-        struct datatype * c = malloc(sizeof(struct datatype)); \
-                                                               \
-        c->type = xdatatype;                                   \
-        c->str  = cstr;                                        \
-                                                               \
-        RB_INSERT(datatype_tree, &datatype_head, c);           \
-} while (0)
-
-
-
 void
 datatype_list_init(void) {
-    if (xdatatype_tree_initialized) {
+    if (ga_xml_datatype_tree_initialized) {
         /* Already initialized. */
         return;
     }
 
-    /* Initializations */
-    xdatatype_add(GA_XACML_DATATYPE_STRING, "http://www.w3.org/2001/XMLSchema#string");
-    xdatatype_add(GA_XACML_DATATYPE_LONG, "http://www.w3.org/2001/XMLSchema#");
-    xdatatype_add(GA_XACML_DATATYPE_INT, "http://www.w3.org/2001/XMLSchema#");
-    xdatatype_add(GA_XACML_DATATYPE_SHORT, "http://www.w3.org/2001/XMLSchema#");
-    xdatatype_add(GA_XACML_DATATYPE_INTEGER, "http://www.w3.org/2001/XMLSchema#integer");
-    xdatatype_add(GA_XACML_DATATYPE_PRIMITIVE, "http://www.w3.org/2001/XMLSchema#primitive");
-    xdatatype_add(GA_XACML_DATATYPE_BOOLEAN, "http://www.w3.org/2001/XMLSchema#boolean");
-    xdatatype_add(GA_XACML_DATATYPE_FLOAT, "http://www.w3.org/2001/XMLSchema#float");
-    xdatatype_add(GA_XACML_DATATYPE_DOUBLE, "http://www.w3.org/2001/XMLSchema#double");
-    xdatatype_add(GA_XACML_DATATYPE_DECIMAL, "http://www.w3.org/2001/XMLSchema#decimal");
-    xdatatype_add(GA_XACML_DATATYPE_DURATION, "http://www.w3.org/2001/XMLSchema#duration");
-    xdatatype_add(GA_XACML_DATATYPE_DATETIME, "http://www.w3.org/2001/XMLSchema#datetime");
-    xdatatype_add(GA_XACML_DATATYPE_TIME, "http://www.w3.org/2001/XMLSchema#time");
-    xdatatype_add(GA_XACML_DATATYPE_DATE, "http://www.w3.org/2001/XMLSchema#date");
-    xdatatype_add(GA_XACML_DATATYPE_GYEARMONTH, "http://www.w3.org/2001/XMLSchema#gyearmonth");
-    xdatatype_add(GA_XACML_DATATYPE_GYEAR, "http://www.w3.org/2001/XMLSchema#gyear");
-    xdatatype_add(GA_XACML_DATATYPE_GMONTHDAY, "http://www.w3.org/2001/XMLSchema#gmonthday");
-    xdatatype_add(GA_XACML_DATATYPE_GDAY, "http://www.w3.org/2001/XMLSchema#gday");
-    xdatatype_add(GA_XACML_DATATYPE_GMONTH, "http://www.w3.org/2001/XMLSchema#gmonth");
-    xdatatype_add(GA_XACML_DATATYPE_HEXBINARY, "http://www.w3.org/2001/XMLSchema#hexbinary");
-    xdatatype_add(GA_XACML_DATATYPE_BASE64BINARY, "http://www.w3.org/2001/XMLSchema#base64binary");
-    xdatatype_add(GA_XACML_DATATYPE_ANYURI, "http://www.w3.org/2001/XMLSchema#anyuri");
-    xdatatype_add(GA_XACML_DATATYPE_QNAME, "http://www.w3.org/2001/XMLSchema#qname");
-    xdatatype_add(GA_XACML_DATATYPE_NOTATION, "http://www.w3.org/2001/XMLSchema#notation");
-    xdatatype_add(GA_XACML_DATATYPE_DERIVED, "http://www.w3.org/2001/XMLSchema#derived");
-    xdatatype_add(GA_XACML_DATATYPE_TOKEN, "http://www.w3.org/2001/XMLSchema#token");
-    xdatatype_add(GA_XACML_DATATYPE_LANGUAGE, "http://www.w3.org/2001/XMLSchema#language");
-    xdatatype_add(GA_XACML_DATATYPE_IDREFS, "http://www.w3.org/2001/XMLSchema#idrefs");
-    xdatatype_add(GA_XACML_DATATYPE_ENTITIES, "http://www.w3.org/2001/XMLSchema#entities");
-    xdatatype_add(GA_XACML_DATATYPE_NMTOKEN, "http://www.w3.org/2001/XMLSchema#nmtoken");
-    xdatatype_add(GA_XACML_DATATYPE_NMTOKENS, "http://www.w3.org/2001/XMLSchema#nmtokens");
-    xdatatype_add(GA_XACML_DATATYPE_NAME, "http://www.w3.org/2001/XMLSchema#name");
-    xdatatype_add(GA_XACML_DATATYPE_NCNAME, "http://www.w3.org/2001/XMLSchema#ncname");
-    xdatatype_add(GA_XACML_DATATYPE_ID, "http://www.w3.org/2001/XMLSchema#id");
-    xdatatype_add(GA_XACML_DATATYPE_IDREF, "http://www.w3.org/2001/XMLSchema#idref");
-    xdatatype_add(GA_XACML_DATATYPE_ENTITY, "http://www.w3.org/2001/XMLSchema#entity");
-    xdatatype_add(GA_XACML_DATATYPE_NONPOSITIVEINTEGER, "http://www.w3.org/2001/XMLSchema#nonpositiveinteger");
-    xdatatype_add(GA_XACML_DATATYPE_NEGATIVEINTEGER, "http://www.w3.org/2001/XMLSchema#negativeinteger");
-    xdatatype_add(GA_XACML_DATATYPE_BYTE, "http://www.w3.org/2001/XMLSchema#byte");
-    xdatatype_add(GA_XACML_DATATYPE_NONNEGATIVEINTEGER, "http://www.w3.org/2001/XMLSchema#nonnegativeinteger");
-    xdatatype_add(GA_XACML_DATATYPE_UNSIGNEDLONG, "http://www.w3.org/2001/XMLSchema#unsignedlong");
-    xdatatype_add(GA_XACML_DATATYPE_UNSIGNEDINT, "http://www.w3.org/2001/XMLSchema#unsignedint");
-    xdatatype_add(GA_XACML_DATATYPE_UNSIGNEDSHORT, "http://www.w3.org/2001/XMLSchema#unsignedshort");
-    xdatatype_add(GA_XACML_DATATYPE_UNSIGNEDBYTE, "http://www.w3.org/2001/XMLSchema#unsignedbyte");
-    xdatatype_add(GA_XACML_DATATYPE_POSITIVEINTEGER, "http://www.w3.org/2001/XMLSchema#positiveinteger");
-    xdatatype_add(GA_XACML_DATATYPE_UNKNOWN, "unknown");
+    RB_INIT(&ga_xml_datatype_head);
 
-    xdatatype_tree_initialized = 1;
+    /* Initializations */
+    xml_datatype_add(GA_XACML_DATATYPE_STRING, "http://www.w3.org/2001/XMLSchema#string");
+    xml_datatype_add(GA_XACML_DATATYPE_LONG, "http://www.w3.org/2001/XMLSchema#");
+    xml_datatype_add(GA_XACML_DATATYPE_INT, "http://www.w3.org/2001/XMLSchema#");
+    xml_datatype_add(GA_XACML_DATATYPE_SHORT, "http://www.w3.org/2001/XMLSchema#");
+    xml_datatype_add(GA_XACML_DATATYPE_INTEGER, "http://www.w3.org/2001/XMLSchema#integer");
+    xml_datatype_add(GA_XACML_DATATYPE_PRIMITIVE, "http://www.w3.org/2001/XMLSchema#primitive");
+    xml_datatype_add(GA_XACML_DATATYPE_BOOLEAN, "http://www.w3.org/2001/XMLSchema#boolean");
+    xml_datatype_add(GA_XACML_DATATYPE_FLOAT, "http://www.w3.org/2001/XMLSchema#float");
+    xml_datatype_add(GA_XACML_DATATYPE_DOUBLE, "http://www.w3.org/2001/XMLSchema#double");
+    xml_datatype_add(GA_XACML_DATATYPE_DECIMAL, "http://www.w3.org/2001/XMLSchema#decimal");
+    xml_datatype_add(GA_XACML_DATATYPE_DURATION, "http://www.w3.org/2001/XMLSchema#duration");
+    xml_datatype_add(GA_XACML_DATATYPE_DATETIME, "http://www.w3.org/2001/XMLSchema#datetime");
+    xml_datatype_add(GA_XACML_DATATYPE_TIME, "http://www.w3.org/2001/XMLSchema#time");
+    xml_datatype_add(GA_XACML_DATATYPE_DATE, "http://www.w3.org/2001/XMLSchema#date");
+    xml_datatype_add(GA_XACML_DATATYPE_GYEARMONTH, "http://www.w3.org/2001/XMLSchema#gyearmonth");
+    xml_datatype_add(GA_XACML_DATATYPE_GYEAR, "http://www.w3.org/2001/XMLSchema#gyear");
+    xml_datatype_add(GA_XACML_DATATYPE_GMONTHDAY, "http://www.w3.org/2001/XMLSchema#gmonthday");
+    xml_datatype_add(GA_XACML_DATATYPE_GDAY, "http://www.w3.org/2001/XMLSchema#gday");
+    xml_datatype_add(GA_XACML_DATATYPE_GMONTH, "http://www.w3.org/2001/XMLSchema#gmonth");
+    xml_datatype_add(GA_XACML_DATATYPE_HEXBINARY, "http://www.w3.org/2001/XMLSchema#hexbinary");
+    xml_datatype_add(GA_XACML_DATATYPE_BASE64BINARY, "http://www.w3.org/2001/XMLSchema#base64binary");
+    xml_datatype_add(GA_XACML_DATATYPE_ANYURI, "http://www.w3.org/2001/XMLSchema#anyuri");
+    xml_datatype_add(GA_XACML_DATATYPE_QNAME, "http://www.w3.org/2001/XMLSchema#qname");
+    xml_datatype_add(GA_XACML_DATATYPE_NOTATION, "http://www.w3.org/2001/XMLSchema#notation");
+    xml_datatype_add(GA_XACML_DATATYPE_DERIVED, "http://www.w3.org/2001/XMLSchema#derived");
+    xml_datatype_add(GA_XACML_DATATYPE_TOKEN, "http://www.w3.org/2001/XMLSchema#token");
+    xml_datatype_add(GA_XACML_DATATYPE_LANGUAGE, "http://www.w3.org/2001/XMLSchema#language");
+    xml_datatype_add(GA_XACML_DATATYPE_IDREFS, "http://www.w3.org/2001/XMLSchema#idrefs");
+    xml_datatype_add(GA_XACML_DATATYPE_ENTITIES, "http://www.w3.org/2001/XMLSchema#entities");
+    xml_datatype_add(GA_XACML_DATATYPE_NMTOKEN, "http://www.w3.org/2001/XMLSchema#nmtoken");
+    xml_datatype_add(GA_XACML_DATATYPE_NMTOKENS, "http://www.w3.org/2001/XMLSchema#nmtokens");
+    xml_datatype_add(GA_XACML_DATATYPE_NAME, "http://www.w3.org/2001/XMLSchema#name");
+    xml_datatype_add(GA_XACML_DATATYPE_NCNAME, "http://www.w3.org/2001/XMLSchema#ncname");
+    xml_datatype_add(GA_XACML_DATATYPE_ID, "http://www.w3.org/2001/XMLSchema#id");
+    xml_datatype_add(GA_XACML_DATATYPE_IDREF, "http://www.w3.org/2001/XMLSchema#idref");
+    xml_datatype_add(GA_XACML_DATATYPE_ENTITY, "http://www.w3.org/2001/XMLSchema#entity");
+    xml_datatype_add(GA_XACML_DATATYPE_NONPOSITIVEINTEGER, "http://www.w3.org/2001/XMLSchema#nonpositiveinteger");
+    xml_datatype_add(GA_XACML_DATATYPE_NEGATIVEINTEGER, "http://www.w3.org/2001/XMLSchema#negativeinteger");
+    xml_datatype_add(GA_XACML_DATATYPE_BYTE, "http://www.w3.org/2001/XMLSchema#byte");
+    xml_datatype_add(GA_XACML_DATATYPE_NONNEGATIVEINTEGER, "http://www.w3.org/2001/XMLSchema#nonnegativeinteger");
+    xml_datatype_add(GA_XACML_DATATYPE_UNSIGNEDLONG, "http://www.w3.org/2001/XMLSchema#unsignedlong");
+    xml_datatype_add(GA_XACML_DATATYPE_UNSIGNEDINT, "http://www.w3.org/2001/XMLSchema#unsignedint");
+    xml_datatype_add(GA_XACML_DATATYPE_UNSIGNEDSHORT, "http://www.w3.org/2001/XMLSchema#unsignedshort");
+    xml_datatype_add(GA_XACML_DATATYPE_UNSIGNEDBYTE, "http://www.w3.org/2001/XMLSchema#unsignedbyte");
+    xml_datatype_add(GA_XACML_DATATYPE_POSITIVEINTEGER, "http://www.w3.org/2001/XMLSchema#positiveinteger");
+    xml_datatype_add(GA_XACML_DATATYPE_UNKNOWN, "unknown");
+
+    ga_xml_datatype_tree_initialized = 1;
 }
 
 const char *
 datatype_to_str(ga_xacml_datatype_t type) {
-    struct datatype   c;
-    struct datatype * found;
+    struct ga_xml_datatype  c;
+    struct ga_xml_datatype *found;
 
     c.type = type;
 
-    if (!(found = RB_FIND(datatype_tree, &datatype_head, &c))) {
+    if (!(found = RB_FIND(ga_xml_datatype_tree, &ga_xml_datatype_head, &c))) {
         return "unknown";
     }
 
